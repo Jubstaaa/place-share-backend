5ed68a4b97c247b620b3d1a538af216629d3281a
İlker Balcılar <ilkerbalcilartr@gmail.com>
2023-03-23 12:39:04 +0300

    update mongoose link

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    `mongodb+srv://jubstaa:GDbs2Cbfqwj0gT2S@cluster0.smqj8of.mongodb.net/place-share?retryWrites=true&w=majority`
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.smqj8of.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
   )
   .then(() => {
     app.listen(process.env.PORT || 9230);
